4937207cc5a094b7d74b6911fb8f0bd1a84cc346
victor <victor.zlatin@eclipso.eu>
2025-05-16 19:52:52 +0300

    tests/lexer

---
+++ src/laba/lexer/lexer_test.go
@@ -0,0 +1,41 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/vzlatin/LABA/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){}`
+	type testToken struct {
+		exectedType     token.TokenType
+		expectedLiteral string
+	}
+
+	tests := []testToken{
+		{token.ASSIGN, "="},
+		{token.LPAREN, "("},
+		{token.PLUS, "+"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.exectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.exectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
